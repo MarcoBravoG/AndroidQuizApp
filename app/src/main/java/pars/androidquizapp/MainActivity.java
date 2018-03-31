a0857b9d15930a06e0590d53b66220718b449416
Samet <sametay153@gmail.com>
2018-03-31 13:10:31 +0300

    first main page design and quit button feature / 2

--- app/src/main/java/pars/androidquizapp/MainActivity.java
+++ app/src/main/java/pars/androidquizapp/MainActivity.java
@@ -2,12 +2,27 @@ package pars.androidquizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button playButton;
+    Button quitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playButton = (Button)findViewById(R.id.playButton);
+        quitButton = (Button) findViewById(R.id.quitButton);
+
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
