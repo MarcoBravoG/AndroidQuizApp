26487cd4ba67803375a5e7d82edf00145cd94477
Samet <sametay153@gmail.com>
2018-03-31 13:40:03 +0300

    play button new intent / category activity / 2

--- app/src/main/java/pars/androidquizapp/MainActivity.java
+++ app/src/main/java/pars/androidquizapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CategoryActivity.class);
+                startActivity(intent);
             }
         });
     }
